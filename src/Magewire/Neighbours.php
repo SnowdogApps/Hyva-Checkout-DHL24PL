6d75357a4a2ee4dc24acc7950ff3a7f5a30472ac
Kamil Balwierz <kamil@snow.dog>
2023-12-19 23:22:42 +0100

    support for neighbor delivery and parcelshops

---
+++ src/Magewire/Neighbours.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Snowdog\Hyva\Checkout\DHL24PL\Magewire;
+
+use Magento\Checkout\Model\Session as SessionCheckout;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magewirephp\Magewire\Component;
+
+class Neighbours extends Component
+{
+    public bool $active = false;
+
+    public string $fullName = '';
+
+    public string $postcode = '';
+
+    public string $city = '';
+
+    public string $street = '';
+
+    public string $houseNumber = '';
+
+    public string $apartmentNumber = '';
+
+    public string $phoneNumber = '';
+
+    public string $email = '';
+
+    public function __construct(
+        private readonly CartRepositoryInterface $quoteRepository,
+        private readonly SessionCheckout         $sessionCheckout
+    ) {
+    }
+
+    public function mount()
+    {
+        $data = $this->sessionCheckout->getDHL24Neighbor();
+        if (is_array($data)) {
+            $this->active = $data['is_neighbours'] === 'true';
+            $this->fullName = $data['neighbor_name'] ?? '';
+            $this->postcode = $data['neighbor_postcode'] ?? '';
+            $this->city = $data['neighbor_city'] ?? '';
+            $this->street = $data['neighbor_street'] ?? '';
+            $this->houseNumber = $data['neighbor_houseNumber'] ?? '';
+            $this->apartmentNumber = $data['neighbor_apartmentNumber'] ?? '';
+            $this->phoneNumber = $data['neighbor_phoneNumber'] ?? '';
+            $this->email = $data['neighbor_emailAddress'] ?? '';
+        }
+    }
+
+    public function updated($value, string $name)
+    {
+        $data = [
+            'is_neighbours' => $this->active ? "true" : "false",
+            'neighbor_name' => $this->fullName,
+            'neighbor_postcode' => $this->postcode,
+            'neighbor_city' => $this->city,
+            'neighbor_street' => $this->street,
+            'neighbor_houseNumber' => $this->houseNumber,
+            'neighbor_apartmentNumber' => $this->apartmentNumber,
+            'neighbor_phoneNumber' => $this->phoneNumber,
+            'neighbor_emailAddress' => $this->email,
+        ];
+
+        $quote = $this->sessionCheckout->getQuote();
+        $quote->setData('dhlpl_neighbor', json_encode($data));
+        $this->quoteRepository->save($quote);
+
+        $this->sessionCheckout->setDHL24Neighbor($data);
+
+        return $value;
+    }
+}
---
+++ src/Magewire/ParcelShop.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Snowdog\Hyva\Checkout\DHL24PL\Magewire;
+
+use Hyva\Checkout\Model\Magewire\Component\EvaluationInterface;
+use Hyva\Checkout\Model\Magewire\Component\EvaluationResultFactory;
+use Hyva\Checkout\Model\Magewire\Component\EvaluationResultInterface;
+use Magento\Checkout\Model\Session as SessionCheckout;
+use Magento\Framework\Url;
+use Magento\Quote\Api\CartRepositoryInterface;
+use Magewirephp\Magewire\Component;
+
+class ParcelShop extends Component implements EvaluationInterface
+{
+    public string $sap = '';
+
+    public string $postcode = '';
+
+    public string $city = '';
+
+    public string $desc = '';
+
+    public function __construct(
+        private readonly CartRepositoryInterface $quoteRepository,
+        private readonly SessionCheckout         $sessionCheckout,
+        private readonly Url                     $url
+    ) {
+    }
+
+    public function evaluateCompletion(EvaluationResultFactory $resultFactory): EvaluationResultInterface
+    {
+        if (
+            !in_array(
+                $this->sessionCheckout->getQuote()->getShippingAddress()->getShippingMethod(),
+                ['dhl_dhl24pl_parcelshop', 'dhl_dhl24pl_parcelshop_cod']
+            )
+        ) {
+            return $resultFactory->createSuccess();
+        }
+
+        if (empty($this->sap)) {
+            return $resultFactory->createBlocking();
+        }
+
+        $quote = $this->sessionCheckout->getQuote();
+        $quote->setData(
+            'dhlpl_parcelshop',
+            json_encode(
+                [
+                    'sap' => $this->sap,
+                    'postcode' => $this->postcode,
+                    'city' => $this->city,
+                ]
+            )
+        );
+        $this->quoteRepository->save($quote);
+
+        return $resultFactory->createSuccess();
+    }
+
+    public function mount()
+    {
+        $data = $this->sessionCheckout->getDHL24Parcelshop();
+        if (is_array($data)) {
+            $this->sap = $data['sap'];
+            $this->postcode = $data['postcode'];
+            $this->city = $data['city'];
+            $this->desc = $data['desc'];
+        }
+    }
+
+    public function parcelshop(string $sap, string $postcode, string $city, string $desc)
+    {
+        $this->sap = $sap;
+        $this->postcode = $postcode;
+        $this->city = $city;
+        $this->desc = $desc;
+
+        $this->sessionCheckout->setDHL24Parcelshop(
+            [
+                'sap' => $sap,
+                'postcode' => $postcode,
+                'city' => $city,
+                'desc' => $desc,
+            ]
+        );
+    }
+
+    public function getMapUrl(): string
+    {
+        $country = $this->sessionCheckout->getQuote()->getShippingAddress()->getCountryId();
+        $params = [];
+
+        if ($country != 'PL') {
+            $params['country'] = $country;
+            $params['type'] = $country == 'DE' ? 'packStation' : 'parcelShop';
+        }
+
+        if ($this->id == 'checkout.shipping.method.dhl_dhl24pl_parcelshop_cod') {
+            $params['type'] = 'lmcod';
+        }
+
+        return $this->url->getUrl('dhl/shipment/lmmap', $params);
+    }
+}
